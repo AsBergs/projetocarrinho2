c44deb394a2846a255b129d4a1270b8888fd491b
AsBergs <anasofiabergsoares06@gmail.com>
2023-05-24 14:16:09 -0300

    COUNTINGMYFOOTSTEPS

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 
-  const newItem = ref({
+const newItem = ref({
   id: 0,
   name: '',
   price: 0,
@@ -10,80 +10,80 @@ import { ref } from 'vue'
 
 })
 
- const products = ref([
-    {
-  
-  
-          id: 1,
-          name: 'SPEAK NOW (TAYLOR`S VERSION) CD',
-          price: 14.99,
-          amount: 0,
-          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/files/z0ZgldjYOzHFAkIXofXa_600x.png'
-          
-      },
-      {
-          id: 2,
-          name: 'SPEAK NOW (TAYLOR`S VERSION) 3LP VINYL',
-          price: 38.99,
-          amount: 0,
-          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/files/3d1vz27EG2yGTIBXSoFr_600x.png'
-      },
-      {
-          id: 3,
-          name: 'SPEAK NOW (TAYLOR`S VERSION) CASSETTE',
-          price: 18.99,
-          amount: 0,
-          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/files/P9OMyRuVtptl46KJEejz_600x.png'
-      },
-      {
+const products = ref([
+  {
 
-        id: 4,
-        name: 'SPEAK NOW (TAYLOR`S VERSION) DIGITAL ALBUM',
-        price: 12.99,
-        amount: 0,
-        image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/files/DczpbBwdBLoOceRfaFk2_600x.png'
 
-      },
-      {
-          id: 5,
-          name: 'MIDNIGHTS: JADE GREEN EDITION CD',
-          price: 12.99,
-          amount: 0,
-          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/CD2_600x.png?v=1665149108'
-          
-        
-      },
-      {
-          id: 6,
-          name: 'MIDNIGHTS: MOONSTONE BLUE EDITION CD',
-          price: 12.99,
-          amount: 0,
-          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/CD1_600x.png'
-          
-      },
-      {
-          id: 7,
-          name: 'MIDNIGHTS: MAHOGANY EDITION CD',
-          price: 12.99,
-          amount: 0,
-          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/CD4_600x.png'
-          
-      },
-      {
-          id: 8,
-          name: 'MIDNIGHTS: BLOOD MOON EDITION CD',
-          price: 12.99,
-          amount: 0,
-          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/CD3_600x.png'
-         
-      }
-  ])
+    id: 1,
+    name: 'SPEAK NOW (TAYLOR`S VERSION) CD',
+    price: 14.99,
+    amount: 0,
+    image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/files/z0ZgldjYOzHFAkIXofXa_600x.png'
 
- const cart = ref([])
+  },
+  {
+    id: 2,
+    name: 'SPEAK NOW (TAYLOR`S VERSION) 3LP VINYL',
+    price: 38.99,
+    amount: 0,
+    image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/files/3d1vz27EG2yGTIBXSoFr_600x.png'
+  },
+  {
+    id: 3,
+    name: 'SPEAK NOW (TAYLOR`S VERSION) CASSETTE',
+    price: 18.99,
+    amount: 0,
+    image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/files/P9OMyRuVtptl46KJEejz_600x.png'
+  },
+  {
+
+    id: 4,
+    name: 'SPEAK NOW (TAYLOR`S VERSION) DIGITAL ALBUM',
+    price: 12.99,
+    amount: 0,
+    image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/files/DczpbBwdBLoOceRfaFk2_600x.png'
+
+  },
+  {
+    id: 5,
+    name: 'MIDNIGHTS: JADE GREEN EDITION CD',
+    price: 12.99,
+    amount: 0,
+    image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/CD2_600x.png?v=1665149108'
+
+
+  },
+  {
+    id: 6,
+    name: 'MIDNIGHTS: MOONSTONE BLUE EDITION CD',
+    price: 12.99,
+    amount: 0,
+    image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/CD1_600x.png'
+
+  },
+  {
+    id: 7,
+    name: 'MIDNIGHTS: MAHOGANY EDITION CD',
+    price: 12.99,
+    amount: 0,
+    image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/CD4_600x.png'
+
+  },
+  {
+    id: 8,
+    name: 'MIDNIGHTS: BLOOD MOON EDITION CD',
+    price: 12.99,
+    amount: 0,
+    image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/CD3_600x.png'
+
+  }
+])
+
+const cart = ref([])
 
 function addToCart(index) {
   if (products.value[index].amount > 0) {
-    newItem.value.id = products.value[index].id;    
+    newItem.value.id = products.value[index].id;
     newItem.value.name = products.value[index].name;
     newItem.value.price = products.value[index].price;
     newItem.value.amount = products.value[index].amount;
@@ -102,15 +102,14 @@ function removeCart(index) {
 }
 
 function takeOut(id) {
-  if (products.value[id]. amount> 0) {
+  if (products.value[id].amount > 0) {
     products.value[id].amount--;
   }
 }
 
 </script>
 <template>
- 
-  <ul class="products-main"> 
+  <ul class="products-main">
     <h1>Taylor Swift Store</h1>
     <h2>Products</h2>
 
@@ -120,7 +119,7 @@ function takeOut(id) {
           <thead>
             <tr>
               <th>Product</th>
-              <th>Price</th> 
+              <th>Price</th>
               <th>Total</th>
             </tr>
           </thead>
@@ -128,9 +127,8 @@ function takeOut(id) {
             <td>
               <div class="product">
                 <img :src="product.image" alt="">
-                <div class="info">
+                <div>
                   <div class="nome">{{ product.name }}</div>
-                 
                 </div>
               </div>
             </td>
@@ -164,26 +162,24 @@ function takeOut(id) {
               <th></th>
             </tr>
           </thead>
-          <tbody class="products-item" v-for="(produto, index) in cart" :key="index">
+          <tbody class="products-item" v-for="(product, index) in cart" :key="index">
             <td>
               <div class="product">
-                <img :src="produto.image" alt="">
+                <img :src="product.image" alt="">
                 <div class="info">
-                  <div class="name">{{ produto.name }}</div>
+                  <div class="name">{{ product.name }}</div>
                 </div>
               </div>
             </td>
-            <td>{{ produto.price }}</td> 
-            <td>{{ (produto.amount * produto.price).toFixed(2) }}</td>
-            <td>{{ produto.amount }}</td>
+            <td>{{ product.price }}</td>
+            <td>{{ (product.amount * product.price).toFixed(2) }}</td>
+            <td>{{ product.amount }}</td>
             <td> <button class="buttonCart" @click="removeCart(index)">Remove</button></td>
           </tbody>
         </table>
       </section>
     </div>
   </div>
-
-
 </template>
 <style scoped>
 
